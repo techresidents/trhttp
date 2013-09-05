255615ea3740f8be457964efe8f493ecbdd56662
Jeffrey Mullins <jmullins@techresidents.com>
2013-09-05 11:56:27 -0400

    Bumping version to 0.5-SNAPSHOT

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def find_packages():
 
 setup(
     name='trhttp',
-    version = '0.4.0',
+    version = '0.5-SNAPSHOT',
     author = 'Tech Residents, Inc.',
     packages = find_packages(),
     license = open('LICENSE').read(),
