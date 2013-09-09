8d4fff2ab2801f710aea7378964b9874c1a7f9ed
Jeffrey Mullins <jmullins@techresidents.com>
2013-09-09 11:57:49 -0400

    Bumping version to 0.7-SNAPSHOT

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ ez_setup.use_setuptools()
 
 setup(
     name='trhttp',
-    version = '0.6.0',
+    version = '0.7-SNAPSHOT',
     author = 'Tech Residents, Inc.',
     packages = find_packages(),
     license = open('LICENSE').read(),
